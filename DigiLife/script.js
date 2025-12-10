91393be329759d352edf89583045561af0acb55e
digilifeluxury <digilifeluxury@gmail.com>
2025-12-09 19:38:01 -0500

    Update script.js

--- DigiLife/script.js
+++ DigiLife/script.js
@@ -10,7 +10,7 @@ function sendMail(event) {
     subject: "Website Contact Form"
 };
 
-    emailjs.send("service_rzy8r2f", "template_tdewbym", parms)
+    emailjs.send("service_rzy8r2f", "template_xqg87eo", parms)
         .then(() => {
             alert("Email Sent!");
             window.location.href = "/thank-you"; 
@@ -93,3 +93,4 @@ function sendMail(event) {
             observer.observe(el);
 
         });
+
